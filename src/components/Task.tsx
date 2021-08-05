a25169a9e6a24b860424e5c111c357a5bcf10e89
Thanawat <thanawat_bumpengpun@cmu.ac.th>
2021-08-05 17:17:20 +0700

    finished todo and task

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,19 +1,38 @@
 type TaskProps = {
     id: number;
     name: string;
+    isDone: boolean;
     doneFn: Function;
     deleteFn: Function;
 }
 
-const Task = ({id, name, doneFn, deleteFn} : TaskProps) => {
-    return (
-        <div className="flex justify-between h-8 items-center py-6 border-b">
-            <span className="text-2xl"> {name} </span>
-            <div className="flex space-x-1 items-center">
-            <button className="bg-green-400 w-24 text-2xl" onClick = { () => doneFn(id) }>Done</button>
-            <button className="bg-red-400 w-24 text-2xl" onClick = { () => deleteFn(id) }>Delete</button>
-            </div>
+const Task = ({id, name,isDone, doneFn, deleteFn} : TaskProps) => {
+
+
+    const taskUndone = <div className="flex justify-between h-8 items-center py-6 border-b group">
+        <span className="text-2xl"> {name} </span>
+    <div className="flex space-x-1 items-center">
+    <button className="bg-green-400 w-24 text-2xl opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out" onClick = { () => doneFn(id) }>Done</button>
+    <button className="bg-red-400 w-24 text-2xl opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out" onClick = { () => deleteFn(id) }>Delete</button>
+    </div>
+    </div>
+
+    const taskDone = <div className="flex justify-between h-8 items-center py-6 border-b">
+        <span className="text-2xl line-through"> {name} </span>
         </div>
+
+    const computeTask = () => {
+        if (isDone === false) {
+            return taskUndone
+        }
+        else {
+            return taskDone
+        }
+    }
+
+
+    return (
+           computeTask()
     )
 }
 
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -5,23 +5,38 @@ import Task from './Task'
 type TaskData = {
     id: number;
     name: string;
+    isDone: boolean;
 }
 
 const TodoList = () => {
 
     const [curTask, setCurTask] = useState<string>('')
     const [tasks, setTasks] = useState<TaskData[]>([])
+    let doned: Array<TaskData>
 
     const onChangeCallback = (ev: React.ChangeEvent<HTMLInputElement>) => {
         setCurTask(ev.target.value)
     }
 
+    const onKeyDownCallback = (ev: React.KeyboardEvent<HTMLInputElement>) => {
+        if (ev.key === "Enter"){
+            addTask(curTask)
+        }
+      }
+
     const addTask = (taskName: string) => {
+        if(taskName === ""){
+            alert("Task cannot be empty")
+        }
+        else{
         const newId = (new Date()).getTime()
 
-        const newTasks = [{id: newId, name: taskName}, ...tasks]
+        const newDone = false;
+
+        const newTasks = [{id: newId, name: taskName, isDone: newDone}, ...tasks]
 
         setTasks(newTasks)
+        }
     }
 
     const deleteTask = (id: number) => {
@@ -30,17 +45,28 @@ const TodoList = () => {
     }
 
     const doneTask = (id: number) => {
-        const newTasks = tasks.filter( x => x.id !== id)
-        setTasks(newTasks)
+        const newTasksUn = tasks.filter( x => x.id !== id)
+        const newTasks = tasks.filter(x => x.id === id)
+        newTasks.forEach(x => {
+            if (x.id === id) {
+                x.isDone = true;
+            }
+        })
+        doned = [ ...newTasks, ...newTasksUn]
+        setTasks(doned)
     }
+
+    const doneTaskNaja = tasks.filter(x => x.isDone === true )
+    const undoneTaskNaja = tasks.filter(x => x.isDone === false)
 
     return (
         <div className='mx-auto max-w-4xl'>
             <div className='flex space-x-1'>
-            <input className='border border-gray-400 w-full text-2xl' onChange={onChangeCallback}></input>
+            <input className='border border-gray-400 w-full text-2xl' onChange={onChangeCallback} onKeyDown={onKeyDownCallback}></input>
             <button className='border border-gray-400 w-8 font-bold' onClick={() => addTask(curTask)}>+</button>
             </div>
-            {tasks.map( x => <Task id={x.id} name={x.name} doneFn={doneTask} deleteFn={deleteTask}/>)}
+            {undoneTaskNaja.map( x => <Task id={x.id} name={x.name} isDone={x.isDone} doneFn={doneTask} deleteFn={deleteTask}/>)}
+            {doneTaskNaja.map( x => <Task id={x.id} name={x.name} isDone={x.isDone} doneFn={doneTask} deleteFn={deleteTask}/>)}
         </div>
     )
 }
